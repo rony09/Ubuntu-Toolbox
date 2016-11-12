a18ed06f5587ae65cca59e3308cf2309d2223dce
Mixih <mxhahn1@gmail.com>
2016-11-12 09:21:03 -0600

    Fix formatting in WinPassChange script

--- Misc Utilities/WindowsPassChange.ps1
+++ Misc Utilities/WindowsPassChange.ps1
@@ -2,6 +2,7 @@
 Write-Host "Run this in a admin powershell!!!"
 Write-Host "USE AT YOUR OWN RISK! I am not responsible to any system harm caused by this script!"
 $passin = Read-Host -Prompt "Enter password that all users will be changed to(WARNING: WRITE THIS DOWN!!!)"
+Write-Host ""
 $userlist = get-wmiobject win32_useraccount
 Foreach ($userob in $userlist) {
     if (-not (($userob.name -Match $env:USERNAME) -or ($userob.name -Match "Guest") -or ($userob.name -Match "DefaultAccount"))) {
@@ -11,9 +12,9 @@ Foreach ($userob in $userlist) {
     } 
     if (($userob.name -Match "Guest") -or ($userob.name -Match "Administrator")) {
         $usernm=$userob.name
-        Write-Host "Disabling $usernm"
+        Write-Host "Disabling account: $usernm"
         ([adsi]("WinNT://"+$userob.caption).replace("\", "/")).psbase.invokeset("AccountDisabled", "True")
         ([adsi]("WinNT://"+$userob.caption).replace("\", "/")).setinfo()
     }
 }
-Write-Host "password change completed!"
+Write-Host "Password change completed!\n"
