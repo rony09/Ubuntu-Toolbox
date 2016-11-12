cc8aaee42d140bb088c6e5846536c462769047df
Mixih <mxhahn1@gmail.com>
2016-11-11 22:02:18 -0600

    Added a few utilites

---
+++ Misc Utilities/WindowsPassChange.ps1
@@ -0,0 +1,19 @@
+﻿Write-Host "Password change/ guest disable script: Dirty programming"
+Write-Host "Run this in a admin powershell!!!"
+Write-Host "USE AT YOUR OWN RISK! I am not responsible to any system harm caused by this script!"
+$passin = Read-Host -Prompt "Enter password that all users will be changed to(WARNING: WRITE THIS DOWN!!!)"
+$userlist = get-wmiobject win32_useraccount
+Foreach ($userob in $userlist) {
+    if (-not (($userob.name -Match $env:USERNAME) -or ($userob.name -Match "Guest") -or ($userob.name -Match "DefaultAccount"))) {
+        $usernm=$userob.name
+        Write-Host "Changing password for $usernm"
+        ([adsi]("WinNT://"+$userob.caption).replace("\", "/")).SetPassword($passin)
+    } 
+    if (($userob.name -Match "Guest") -or ($userob.name -Match "Administrator")) {
+        $usernm=$userob.name
+        Write-Host "Disabling $usernm"
+        ([adsi]("WinNT://"+$userob.caption).replace("\", "/")).psbase.invokeset("AccountDisabled", "True")
+        ([adsi]("WinNT://"+$userob.caption).replace("\", "/")).setinfo()
+    }
+}
+Write-Host "password change completed!"
